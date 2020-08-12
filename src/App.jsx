bd29876e543645cea790fbf8222e44cd20503f9f
lilpolymath <ayofavour7@gmail.com>
2020-08-12 10:01:41 +0100

    feat: components overview

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import SidebarItem from './components/SidebarItem';
 function App() {
   return (
     <div className='App'>
-      <h3>Welcome to Quidax</h3>
-      <div className='test'>
+      <h3 className='component_header'>App Components</h3>
+      <hr></hr>
+      <div className='components'>
         <SidebarItem name='Profile' active={true} badge='3' />
         <SidebarItem name='Profile' badge='3' />
         <SidebarItem name='Home' />
